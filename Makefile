9aef3c48422db6575f64eff15548fc7e8b9c16d8
lmells98 <leighton.mells23@gmail.com>
2023-07-18 21:42:17 +0930

    Added Debug Build & Added .vscode hidden dir to gitignore

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 SHELL := bash
 
+DEBUG = 0
+
 SRC = src/*.c
 INC = inc
 LIB = lib
@@ -18,29 +20,59 @@ LINK_LIBS = -L$(LIBFT_DIR) -lft -L$(MLX_DIR) -lmlx
 HEADERS = -I$(INC) -I$(LIBFT_DIR) -I$(GNL_DIR) -I$(MLX_DIR)
 FLAGS = -Wall -Werror -Wextra $(LINK_LIBS) $(HEADERS) -framework OpenGL -framework AppKit
 
+ifeq ($(DEBUG), 1)
+	FLAGS+=-g
+	GNL_COMP_FLAGS+=-g
+endif
+
 all: $(LIBS)
-	@gcc $(SRC) $(FLAGS) -o cub3d
+ifeq ($(DEBUG), 1)
+	cc $(FLAGS) $(SRC) -o cub3d
+else
+	@cc $(FLAGS) $(SRC) -o cub3d
 	@echo Good to go
+endif
 	
 $(LIBS):
 	@clear
-	@echo Compiling libft
+ifeq ($(DEBUG), 1)
+	@make -C $(LIBFT_DIR) DEBUG=1
+else
+	@printf "Compiling libft\t\t\t"
 	@make -s -C $(LIBFT_DIR)
+	@printf "\e[0;92mDONE!\e[0m\n"
+endif
 
-	@echo Compiling get_next_line
+ifeq ($(DEBUG), 1)
 	@-for i in $(GNL_SRC:%.c=%); do\
-		gcc $$i.c $(GNL_COMP_FLAGS) -c -o $$i.o;\
+		echo "cc $$i.c $(GNL_COMP_FLAGS) -c -o $$i.o";\
+		cc $$i.c $(GNL_COMP_FLAGS) -c -o $$i.o;\
 	done
-	@ar rcs $(LIBFT_DIR)/libft.a $(GNL_OBJ)
+	ar -rcsv $(LIBFT_DIR)/libft.a $(GNL_OBJ)
+else
+	@printf "Compiling get next line\t\t"
+	@-for i in $(GNL_SRC:%.c=%); do\
+		cc $$i.c $(GNL_COMP_FLAGS) -c -o $$i.o;\
+	done
+	@ar -rcs $(LIBFT_DIR)/libft.a $(GNL_OBJ)
+	@printf "\e[0;92mDONE!\e[0m\n"
+endif
 
-	echo Compiling mlx
-	make -s -C $(MLX_DIR)
+ifeq ($(DEBUG), 1)
+	@make -C $(MLX_DIR) DEBUG=1
+else
+	@printf "Compiling mlx\t\t\t"
+	@make -s -C $(MLX_DIR)
+	@printf "\e[0;92mDONE!\e[0m\n"
+endif
 
 clean:
 	@clear
+	@rm -rf *.dSYM
 	@rm -f cub3d
 	@rm -f $(GNL_DIR)/*.o
 	@make fclean -s -C $(LIBFT_DIR)
+	@make clean -s -C $(MLX_DIR)
 	@echo nothing here anymore, well, except .c
 
 fclean: clean
--- src/cub3d.c
+++ src/cub3d.c
@@ -1,32 +1,71 @@
-#include "mlx.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "cub3d.h"
 
-typedef struct s_window_manager
-{
-	void	*mlx;
-	void	*window;
-}	t_window;
+#define TEXT_RED "\e[0;91m"
+#define TEXT_WHITE "\e[0m"
 
-int	close_mlx_window(t_window *window)
+int	close_mlx_window(t_mlx_data *mlx)
 {
-	mlx_destroy_window(window->mlx, window->window);
+	mlx_destroy_window(mlx->data, mlx->window);
 	exit(1);
 	return (0);
 }
 
-int main()
+int	key_press(int keycode, t_mlx_data *mlx)
 {
-	t_window	window;
+	if (MACOS)
+	{
+		if (keycode == KEY_ESC)
+			return (close_mlx_window(mlx));
+		printf("NOT YET IMPLEMENTED!\n");
+	}
+	return (keycode);
+}
 
-	window.mlx = mlx_init();
-	window.window = mlx_new_window(window.mlx, 1920, 1080, "Hello world!");
-	if (!window.window)
+static bool	validate_args(int c, char **v)
+{
+	char	*file_extension;
+
+	if (c != 2)
+	{
+		write(STDERR_FILENO, TEXT_RED "Error", ft_strlen(TEXT_RED "Error"));
+		write(STDERR_FILENO, TEXT_WHITE " : Incorrect number of arguments.\n",
+			ft_strlen(TEXT_WHITE " : Incorrect number of arguments.\n"));
+		return (false);
+	}
+	file_extension = ft_strrchr(v[1], '.');
+	if (!file_extension
+		|| ft_strncmp(".cub", file_extension, ft_strlen(".cub")))
+	{
+		write(STDERR_FILENO, TEXT_RED "Error", ft_strlen(TEXT_RED "Error"));
+		write(STDERR_FILENO, TEXT_WHITE " : \"", ft_strlen(TEXT_WHITE " : \""));
+		write(STDERR_FILENO, v[1], ft_strlen(v[1]));
+		write(STDERR_FILENO, "\" is not a '.cub' map file.\n",
+			ft_strlen("\" is not a '.cub' map file.\n"));
+		return (false);
+	}
+	return (true);
+}
+
+void	initialise_mlx(t_mlx_data *mlx)
+{
+	mlx->data = mlx_init();
+	mlx->window = mlx_new_window(mlx->data, WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
+	if (!mlx->window)
 	{
 		fprintf(stderr, "\e[0;91mError\e[0m : Failed to create a new window.\n");
-		return (1);
+		exit(1);
 	}
-	mlx_hook(window.window, 17, 0, close_mlx_window, &window);
-	mlx_loop(window.mlx);
+	mlx_key_hook(mlx->window, key_press, mlx);
+	mlx_hook(mlx->window, 17, 0, close_mlx_window, mlx);
+}
+
+int	main(int c, char **v)
+{
+	t_cub3d	app;
+
+	if (!validate_args(c, v))
+		return (1);
+	initialise_mlx(&app.mlx);
+	mlx_loop(app.mlx.data);
 	return (0);
 }
