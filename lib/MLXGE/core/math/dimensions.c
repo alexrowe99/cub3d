f43b7dc56f35013111d384144cfbeb5b946e36ca
lmells98 <leighton.mells23@gmail.com>
2023-12-13 11:24:54 +1030

    Organised math types

--- lib/MLXGE/core/math/dimensions.c
+++ lib/MLXGE/core/math/dimensions.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.h                                           :+:      :+:    :+:   */
+/*   dimensions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmells <lmells@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 17:58:53 by lmells            #+#    #+#             */
-/*   Updated: 2023/12/13 10:58:31 by lmells           ###   ########.fr       */
+/*   Created: 2023/12/13 10:54:38 by lmells            #+#    #+#             */
+/*   Updated: 2023/12/13 11:15:24 by lmells           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_H
-# define VECTOR_H
+#include <dimensions.h>
 
-typedef struct s_vector_2_double
+t_dimensions    dimension_2d(int w, int h)
 {
-	double	x;
-	double	y;
-}	t_v2d;
-
-typedef struct s_vector_2_int
-{
-	int		x;
-	int		y;
-}	t_v2i;
-
-#endif
-
-t_v2d   vector_2_double(double x, double y);
-t_v2i   vector_2_int(int x, int y);
+    return ((t_dimensions){w, h});
+}
--- lib/MLXGE/core/math/vector.c
+++ lib/MLXGE/core/math/vector.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.h                                           :+:      :+:    :+:   */
+/*   vector.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmells <lmells@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 17:58:53 by lmells            #+#    #+#             */
-/*   Updated: 2023/12/13 10:58:31 by lmells           ###   ########.fr       */
+/*   Created: 2023/12/13 10:54:07 by lmells            #+#    #+#             */
+/*   Updated: 2023/12/13 11:15:07 by lmells           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_H
-# define VECTOR_H
+#include <vector.h>
 
-typedef struct s_vector_2_double
+t_v2d   vector_2_double(double x, double y)
 {
-	double	x;
-	double	y;
-}	t_v2d;
+    return ((t_v2d){x, y});
+}
 
-typedef struct s_vector_2_int
+t_v2i   vector_2_int(int x, int y)
 {
-	int		x;
-	int		y;
-}	t_v2i;
-
-#endif
-
-t_v2d   vector_2_double(double x, double y);
-t_v2i   vector_2_int(int x, int y);
+    return ((t_v2i){x, y});
+}
